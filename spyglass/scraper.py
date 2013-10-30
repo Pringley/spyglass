767f0a37279858b2694ac65bfdecb472e9e57b32
Ben Pringle <ben.pringle@gmail.com>
2013-10-29 22:11:09 -0400

    Fix cache bug where torrent info always fetched
    The top() function was passing a None default value to the torrent
    cache, when it should be using the Scraper object's default value if
    None is supplied.

--- spyglass/scraper.py
+++ spyglass/scraper.py
@@ -7,14 +7,15 @@ from .torrent import Torrent
 class Scraper:
 
     def top(self, n=10, cache=None, prefetch=False):
-        if self._use_cache(cache) and len(self._top_cache) >= n:
+        use_cache = self._use_cache(cache)
+        if use_cache and len(self._top_cache) >= n:
             return self._top_cache[:n]
         soup = get(TOP).soup
         links = soup.find_all("a", class_="detLink")
         urls = [urlparse.urljoin(TOP, link.get('href')) for link in links]
-        torrents = [self.torrent_from_url(url, cache, prefetch)
+        torrents = [self.torrent_from_url(url, use_cache, prefetch)
                 for url in urls]
-        if self._use_cache(cache):
+        if use_cache:
             self._top_cache = torrents
             self._add_to_torrent_cache(torrents)
         return torrents[:n]
